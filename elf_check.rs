6235400d9bbc6664ba9a3e090f8ec909234350ed
mahdikohee <mahdikohee@gmail.com>
2025-05-10 09:10:39 +0000

    Update elf_check.rs

--- elf_check.rs
+++ elf_check.rs
@@ -2,9 +2,13 @@ use goblin::Object;
 use std::fs;
 
 fn main() {
-    let bytes = fs::read("main").expect("Cannot read file"); // use compiled file insted of main file 
-    match Object::parse(&bytes).unwrap() {
-        Object::Elf(_) => println!("This is an ELF file."),
-        _ => println!("Not an ELF file."),
+    let bytes = fs::read("some_binary").expect("Cannot read file");
+
+    match Object::parse(&bytes) {
+        Ok(obj) => match obj {
+            Object::Elf(_) => println!("This is an ELF file."),
+            _ => println!("Not an ELF file."),
+        },
+        Err(e) => println!("Error parsing object: {}", e),
     }
 }
