2ff6030953b669940c094a2daf22b19e4a330905
mahdikohee <mahdikohee@gmail.com>
2025-05-12 08:05:19 -0600

    Create basic_skills.rs

---
+++ basic_skills.rs
@@ -0,0 +1,17 @@
+//Practise of "Some" and "None" 
+fn maybe_get_number(get_it: bool) -> Option<i32> {
+    if get_it {
+        Some(42)
+    } else {
+        None
+    }
+}
+
+fn main() {
+    let result = maybe_get_number(true);
+
+    match result {
+        Some(n) => println!("Got a number: {}", n),
+        None => println!("Got nothing"),
+    }
+}
