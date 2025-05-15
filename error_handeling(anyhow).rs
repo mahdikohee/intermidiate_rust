7f0e509c5e24c3f5a25e4b892f871ccfbd9592ed
mahdikohee <mahdikohee@gmail.com>
2025-05-15 05:21:16 -0400

    Update error_handeling(anyhow).rs

--- error_handeling(anyhow).rs
+++ error_handeling(anyhow).rs
@@ -1,3 +1,4 @@
+///ex for learning error handeling and returning value 
 use anyhow::{Result, anyhow};
 
 fn might_fail(i: i32) -> Result<String> {
@@ -14,3 +15,37 @@ fn main() -> Result<()> {        //here use only Result<()> not std::io::Result;
     println!("Success: {}", val);
     Ok(())
 }
+
+//ex -2 
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    let result = sum(10, 20);
+    println!("Sum is: {}", result);
+}
+
+//ex-3 
+fn find_even_number(numbers: &[i32]) -> Option<i32> {
+    for num in numbers {
+        if num % 2 == 0 {
+            return Some(*num);
+        }
+    }
+    None
+}
+
+fn main() {
+    let nums = vec![1, 3, 7, 10];
+    if let Some(even) = find_even_number(&nums) {
+        println!("First even number: {}", even);
+    } else {
+        println!("No even number found");
+    }
+}
+
+
+
+ex-3 
+
