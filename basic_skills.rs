edfdea1f7aed0358e3e4bd030736b87e43a410dc
mahdikohee <mahdikohee@gmail.com>
2025-05-27 09:37:36 +0600

    Update basic_skills.rs

--- basic_skills.rs
+++ basic_skills.rs
@@ -15,3 +15,21 @@ fn main() {
         None => println!("Got nothing"),
     }
 }
+
+//some vec 
+fn main() {
+    let vector = vec![1, 2, 3, 4, 5];
+
+    // রেফারেন্স দিয়ে সরাসরি ইন্ডেক্সে এক্সেস
+    let third_element: &i32 = &vector[2];
+    println!("The third element is: {:?}", third_element);
+
+    // .get() রিটার্ন করে Option<&i32>
+    let third_element: Option<&i32> = vector.get(2);
+    match third_element {
+        Some(value) => println!("The third element is: {:?}", value),
+        None => println!("There is no third element!"),
+    }
+}
+
+
