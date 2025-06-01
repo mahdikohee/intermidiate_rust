4ad3db0a055e4293e6be9e2e4d3c888a5cb34b56
mahdikohee <mahdikohee@gmail.com>
2025-06-01 10:27:47 +0600

    Update basic_skills.rs

--- basic_skills.rs
+++ basic_skills.rs
@@ -156,3 +156,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+
+////another wau to get mac address 
+use mac_address::get_mac_address;
+
+fn main() {
+    match get_mac_address() {
+        Ok(Some(ma)) => {
+            println!("MAC addr = {}", ma);
+            println!("bytes = {:?}", ma.bytes());
+        }
+        Ok(None) => println!("No MAC address found."),
+        Err(e) => println!("{:?}", e),
+    }
+}
+
+//
