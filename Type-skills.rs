8f0896d6a7c5243b80badb4f3e9e6a506241c1f0
mahdikohee <mahdikohee@gmail.com>
2025-06-02 18:00:11 +0600

    Update and rename u32.rs to Type-skills.rs

---
+++ Type-skills.rs
@@ -0,0 +1,2 @@
+//rust static typing  skills is extremely important for reverse engenering like code breaking and sym exe 
+//u32 of rust 
--- u32.rs
+++
@@ -1,7 +0,0 @@
-///example of u32 
-fn main() {
-    println!("u32 min: {}", u32::MIN);
-    println!("u32 max: {}", u32::MAX);
-}
-
-////another example 
