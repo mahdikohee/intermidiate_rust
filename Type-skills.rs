91fbf082a52c5a4ee8d3d6a1a517c60cd19088e1
mahdikohee <mahdikohee@gmail.com>
2025-06-03 16:07:55 +0600

    Update Type-skills.rs

--- Type-skills.rs
+++ Type-skills.rs
@@ -1,2 +1,21 @@
 //rust static typing  skills is extremely important for reverse engenering like code breaking and sym exe 
 //u32 of rust 
+fn main() {
+    let n = 0b01001100u32;
+    println!("{}", n.count_ones()); // 3
+
+    let max = u32::MAX;
+    println!("{}", max.count_ones()); // 32
+
+    let zero = 0u32;
+    println!("{}", zero.count_ones()); // 0
+}
+
+//u32 for to count zeros 
+fn main() {
+    let zero = 0u32;
+    println!("{}", zero.count_zeros()); // 32
+
+    let max = u32::MAX;
+    println!("{}", max.count_zeros()); // 0
+}
