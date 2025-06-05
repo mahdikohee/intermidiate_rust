831b9c0fe1febd0961c497a3b929e4fb86e66767
mahdikohee <mahdikohee@gmail.com>
2025-06-05 09:21:57 +0600

    Update rust_oop.rs

--- rust_oop.rs
+++ rust_oop.rs
@@ -1,3 +1,6 @@
+//I still suck at Rust OOP.
+//A simple example of rust oop
+//ex-1 
 trait Show {
     fn show(&self) -> String;
 }
@@ -20,4 +23,30 @@ fn main() {
 
     println!("{}", num1.show()); // Output: four-byte signed 42
     println!("{}", num2.show()); // Output: eight-byte float 3.14
+}
+
+//ex2
+// Trait definition
+trait Greet {
+    fn greet(&self);
+}
+
+// Struct
+struct Person {
+    name: String,
+}
+
+// Trait implement for struct
+impl Greet for Person {
+    fn greet(&self) {
+        println!("Hello, {}!", self.name);
+    }
+}
+
+// main function to run
+fn main() {
+    let person = Person {
+        name: String::from("Rust Learner"),
+    };
+    person.greet();
 }
