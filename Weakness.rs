ba4189bd545a4c4b05f6eb4c583eec856a5d861e
mahdikohee <mahdikohee@gmail.com>
2025-06-06 22:08:00 +0600

    Create Weakness.rs

---
+++ Weakness.rs
@@ -0,0 +1,18 @@
+While i exploring rust ......I discover my weakness and my lackness 
+those are things that i must learn before my project 
+
+
+
+0x1 => https://stevedonovan.github.io/rust-gentle-intro/object-orientation.html                              
+This page contains a lot of oop example that i must learn first 
+
+0x2 => https://stevedonovan.github.io/rust-gentle-intro/pain-points.html 
+There are a lot of pain ...But i must 
+
+0x3 => https://doc.rust-lang.org/reference/introduction.html  
+And this is a long journey 
+0x4 => https://doc.rust-lang.org/std/keyword.type.html
+Rust Type keyword 
+
+0x5 => https://doc.rust-lang.org/std/iter/
+and finally this long path ......finish iter with all releted things ....\
