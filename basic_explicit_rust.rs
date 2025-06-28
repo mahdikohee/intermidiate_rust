33bbbb4b5da42399f554d7e16edc6b839d7fa37f
mahdikohee <mahdikohee@gmail.com>
2025-06-28 20:33:04 +0600

    Update basic_explicit_rust.rs

--- basic_explicit_rust.rs
+++ basic_explicit_rust.rs
@@ -1,1 +1,2 @@
 -------------this is for to understand all and everything of explicit typing of rust-----------------------
+target => fuck cpp , -000000.1% bug , verbose code to understand what is hapenning , and mainly make rust without any hidden controll like zig and c 
