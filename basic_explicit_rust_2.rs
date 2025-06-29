156517a8e19f4f9ef1562d2f5827233b518cd0e6
mahdikohee <mahdikohee@gmail.com>
2025-06-29 12:06:42 +0600

    Update basic_explicit_rust_2.rs

--- basic_explicit_rust_2.rs
+++ basic_explicit_rust_2.rs
@@ -1,2 +1,13 @@
 --In this file i will do all rust code form the first but in extreme explicit way 
-
+//A simple vector and for loop 
+use std::vec::Vec ;
+use std::ops::RangeInclusive ;
+fn main(){
+    let mut vector : Vec<i32> = Vec::<i32>::new() ;
+    let num_range : RangeInclusive<i32> = RangeInclusive::<i32>::new(1i32 ,10i32) ;
+    for i in num_range{
+        let double : i32 = i * 2 ;
+        Vec::<i32>::push(&mut vector , double) ;
+    }
+    println!("Your vector is {:?}" , vector) ;
+}
